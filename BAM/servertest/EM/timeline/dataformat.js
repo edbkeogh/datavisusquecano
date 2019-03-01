05ab12ba1e29c09f92b542c5524ec81709a90713
Edward Keogh <edbkeogh@gmail.com>
2019-03-01 02:57:24 -0600

    demo of timeline, need to talk about how this is organized

--- BAM/servertest/EM/timeline/dataformat.js
+++ BAM/servertest/EM/timeline/dataformat.js
@@ -1,68 +1,157 @@
 var tldata = [
   {
-    group: "group1name",
+    group: "Medinet Habu, Egypt",
     data: [
       {
-        label: "label1name",
+        label: "Great Harris Papyrus (Longest surviving ancient Egyptian papyrus)",
         data: [
           {
-            timeRange: [Date('December 17, 1995 03:24:00'), Date('December 19, 1995 03:24:00')],
-            val: 'test'//<val: number (continuous dataScale) or string (ordinal dataScale)>
-          },
-          {
-            timeRange: [Date('December 17, 1985 03:24:00'), Date('December 19, 1992 03:24:00')],
-            val: 'test2'
-          }
+            timeRange: [-1170, -1160],
+            val: 'Papyrus'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+          }//,
+          // {
+          //   timeRange: [new Date('December 17, 1985 03:24:00'), new Date('December 19, 1992 03:24:00')],
+          //   val: 'test2'
+          // }
         //  (...)
         ]
-      },
-      {
-        label: "label2name",
-        data:   {
-            timeRange: [Date('January 17, 1995 03:24:00'), Date('December 19, 1995 03:24:00')],
-            val: 'test3'//<val: number (continuous dataScale) or string (ordinal dataScale)>
-          },
-          {
-            timeRange: [Date('January 17, 1985 03:24:00'), Date('May 19, 1992 03:24:00')],
-            val: 'test4'
-          }
-        //  (...)
-      // },
-      // (...)
-    ],
-  },
+      }//,
+    //   {
+    //     label: "label2name",
+    //     data:   [
+    //
+    //     {
+    //         timeRange: [new Date('January 17, 1995 03:24:00'), new Date('December 19, 1995 03:24:00')],
+    //         val: 'test3'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+    //       },
+    //       {
+    //         timeRange: [new Date('January 17, 1985 03:24:00'), new Date('May 19, 1992 03:24:00')],
+    //         val: 'test4'
+    //       }
+    //     //  (...)
+    //   ],
+    //   // (...)
+    // },
+  ],
+},
+{
+  group: "Bamiyan, Afghanistan",
+  data: [
+    {
+      label: "Bower Manuscript",
+      data: [
+        {
+          timeRange: [300, 600],
+          val: 'Birchbark'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    }
+],
+},
   {
-    group: "group2name",
+    group: "Peshawar, Pakistan",
     data: [
       {
-        label: "label3name",
+        label: "Irk Bitig/Book of Omens",
         data: [
           {
-            timeRange: [Date('December 17, 1995 03:24:00'), Date('December 19, 1995 03:24:00')],
-            val: 'test5'//<val: number (continuous dataScale) or string (ordinal dataScale)>
-          },
-          {
-            timeRange: [Date('December 17, 1985 03:24:00'), Date('December 19, 1992 03:24:00')],
-            val: 'test6'
+            timeRange: [800, 1000],
+            val: 'Paper'//<val: number (continuous dataScale) or string (ordinal dataScale)>
           }
         //  (...)
         ]
       },
       {
-        label: "label4name",
-        data:   {
-            timeRange: [Date('January 17, 1995 03:24:00'), Date('December 19, 1995 03:24:00')],
-            val: 'test7'//<val: number (continuous dataScale) or string (ordinal dataScale)>
-          },
+        label: "Old Tibetan Annals (Chinese Buddhist Sutra on Reverse)",
+        data:   [
           {
-            timeRange: [Date('January 17, 1985 03:24:00'), Date('May 19, 1992 03:24:00')],
-            val: 'test8'
+            timeRange: [850, 1100],
+            val: 'Paper'//<val: number (continuous dataScale) or string (ordinal dataScale)>
           }
-        //  (...)
-      // },
-      // (...)
     ]
   },
+  {
+    label: "Chinese Paper Scroll of Lotus Sutra",
+    data:   [
+      {
+        timeRange: [618, 906],
+        val: 'Paper'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+      }
+]
+},
   // (...)
+]
+},
+{
+  group: "Gandhara, Pakistan",
+  data: [
+    {
+      label: "Bamiyan Manuscripts (ca. 5,000 fragments from ca. 1,000 manuscripts)",
+      data: [
+        {
+          timeRange: [100, 800],
+          val: 'Palm Leaf; Birchbark; Leather'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    }
+],
+},
+{
+  group: "Gilgit, Pakistan",
+  data: [
+    {
+      label: "Gilgit Manuscripts (approximately 62, not fully catalogued)",
+      data: [
+        {
+          timeRange: [400, 700],
+          val: 'Birchbark; Paper'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    }
+],
+},
+{
+  group: "Kucha, China",
+  data: [
+    {
+      label: "Bakshali Manuscript",
+      data: [
+        {
+          timeRange: [800, 1200],
+          val: 'Fragments'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    }
+],
+},
+{
+  group: "Dunhuang, China",
+  data: [
+    {
+      label: "Gandharan Buddhist Manuscripts",
+      data: [
+        {
+          timeRange: [0, 150],
+          val: 'Birchbark'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    }
+],
+},
+{
+  group: "Khotan, China",
+  data: [
+    {
+      label: "Khotan Dharmapada/Gandhari Dharmapada",
+      data: [
+        {
+          timeRange: [100, 200],
+          val: 'Birchbark'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    }
+],
+},
+
 ]
 ;
