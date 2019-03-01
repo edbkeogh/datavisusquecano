01af45f8af48d59056a389d8ad2f2ecdbb5fb3d1
Edward Keogh <edbkeogh@gmail.com>
2019-03-01 03:11:43 -0600

    findspot variant granularity

---
+++ BAM/servertest/EM/timeline/dataformat2.js
@@ -0,0 +1,139 @@
+var tldata = [
+  {
+    group: "Egypt",
+    data: [
+      {
+        label: "Great Harris Papyrus (Longest surviving ancient Egyptian papyrus)",
+        data: [
+          {
+            timeRange: [-1170, -1160],
+            val: 'Papyrus'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+          }//,
+          // {
+          //   timeRange: [new Date('December 17, 1985 03:24:00'), new Date('December 19, 1992 03:24:00')],
+          //   val: 'test2'
+          // }
+        //  (...)
+        ]
+      }//,
+    //   {
+    //     label: "label2name",
+    //     data:   [
+    //
+    //     {
+    //         timeRange: [new Date('January 17, 1995 03:24:00'), new Date('December 19, 1995 03:24:00')],
+    //         val: 'test3'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+    //       },
+    //       {
+    //         timeRange: [new Date('January 17, 1985 03:24:00'), new Date('May 19, 1992 03:24:00')],
+    //         val: 'test4'
+    //       }
+    //     //  (...)
+    //   ],
+    //   // (...)
+    // },
+  ],
+},
+{
+  group: "Afghanistan",
+  data: [
+    {
+      label: "Bower Manuscript",
+      data: [
+        {
+          timeRange: [300, 600],
+          val: 'Birchbark'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    }
+],
+},
+  {
+    group: "Pakistan",
+    data: [
+      {
+        label: "Irk Bitig/Book of Omens",
+        data: [
+          {
+            timeRange: [800, 1000],
+            val: 'Paper'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+          }
+        //  (...)
+        ]
+      },
+      {
+        label: "Old Tibetan Annals (Chinese Buddhist Sutra on Reverse)",
+        data:   [
+          {
+            timeRange: [850, 1100],
+            val: 'Paper'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+          }
+    ]
+  },
+  {
+    label: "Chinese Paper Scroll of Lotus Sutra",
+    data:   [
+      {
+        timeRange: [618, 906],
+        val: 'Paper'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+      }
+]
+},
+{
+  label: "Bamiyan Manuscripts (ca. 5,000 fragments from ca. 1,000 manuscripts)",
+  data: [
+    {
+      timeRange: [100, 800],
+      val: 'Palm Leaf; Birchbark; Leather'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+    }
+  ]
+},
+
+{
+  label: "Gilgit Manuscripts (approximately 62, not fully catalogued)",
+  data: [
+    {
+      timeRange: [400, 700],
+      val: 'Birchbark; Paper'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+    }
+  ]
+}
+  // (...)
+]
+},
+{
+  group: "China",
+  data: [
+    {
+      label: "Bakshali Manuscript",
+      data: [
+        {
+          timeRange: [800, 1200],
+          val: 'Fragments'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+      ]
+    },
+    {
+      label: "Gandharan Buddhist Manuscripts",
+      data: [
+        {
+          timeRange: [0, 150],
+          val: 'Birchbark'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    },
+    {
+      label: "Khotan Dharmapada/Gandhari Dharmapada",
+      data: [
+        {
+          timeRange: [100, 200],
+          val: 'Birchbark'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    }
+],
+},
+
+
+]
+;
