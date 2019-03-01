fb4aa7cf1f3fd2138e336c295d368dc722045ee6
Edward Keogh <edbkeogh@gmail.com>
2019-03-01 00:28:08 -0600

    timeline testing

---
+++ BAM/servertest/EM/timeline/dataformat.js
@@ -0,0 +1,68 @@
+var tldata = [
+  {
+    group: "group1name",
+    data: [
+      {
+        label: "label1name",
+        data: [
+          {
+            timeRange: [Date('December 17, 1995 03:24:00'), Date('December 19, 1995 03:24:00')],
+            val: 'test'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+          },
+          {
+            timeRange: [Date('December 17, 1985 03:24:00'), Date('December 19, 1992 03:24:00')],
+            val: 'test2'
+          }
+        //  (...)
+        ]
+      },
+      {
+        label: "label2name",
+        data:   {
+            timeRange: [Date('January 17, 1995 03:24:00'), Date('December 19, 1995 03:24:00')],
+            val: 'test3'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+          },
+          {
+            timeRange: [Date('January 17, 1985 03:24:00'), Date('May 19, 1992 03:24:00')],
+            val: 'test4'
+          }
+        //  (...)
+      // },
+      // (...)
+    ],
+  },
+  {
+    group: "group2name",
+    data: [
+      {
+        label: "label3name",
+        data: [
+          {
+            timeRange: [Date('December 17, 1995 03:24:00'), Date('December 19, 1995 03:24:00')],
+            val: 'test5'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+          },
+          {
+            timeRange: [Date('December 17, 1985 03:24:00'), Date('December 19, 1992 03:24:00')],
+            val: 'test6'
+          }
+        //  (...)
+        ]
+      },
+      {
+        label: "label4name",
+        data:   {
+            timeRange: [Date('January 17, 1995 03:24:00'), Date('December 19, 1995 03:24:00')],
+            val: 'test7'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+          },
+          {
+            timeRange: [Date('January 17, 1985 03:24:00'), Date('May 19, 1992 03:24:00')],
+            val: 'test8'
+          }
+        //  (...)
+      // },
+      // (...)
+    ]
+  },
+  // (...)
+]
+;
