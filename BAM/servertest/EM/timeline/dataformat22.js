dc9179d51ef7e140496d5cdf14fda9a9b3eeb96b
edbkeogh <35552750+edbkeogh@users.noreply.github.com>
2019-05-02 12:26:37 -0500

    new style timeline
    new timeline demo one region

---
+++ BAM/servertest/EM/timeline/dataformat22.js
@@ -0,0 +1,103 @@
+var tldata = [
+  {
+    group: "China",
+    data: [
+      {
+        label: "China",
+        data: [
+          {
+            timeRange: [-1600, -1046],
+            val: 'Shang'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+          },
+          {
+            timeRange: [-1046, -256],
+            val: 'Zhou'
+          },
+          {
+            timeRange: [-221, -207],
+            val: 'Qin'
+          },
+          {
+            timeRange: [-202, 220],
+            val: 'Han'
+          },
+          {
+            timeRange: [226, 420],
+            val: 'Jin'
+          },
+
+          {
+            timeRange: [581, 618],
+            val: 'Sui'
+          },
+          {
+            timeRange: [618, 907],
+            val: 'Tang'
+          },
+          {
+            timeRange: [907, 960],
+            val: 'Five Dynasties'
+          },
+          {
+            timeRange: [960, 1234],
+            val: 'Song'
+          },
+          {
+            timeRange: [1271, 1368],
+            val: 'Yuan'
+          },
+          {
+            timeRange: [1368, 1644],
+            val: 'Ming'
+          }
+          //,
+          // {
+          //   timeRange: [new Date('December 17, 1985 03:24:00'), new Date('December 19, 1992 03:24:00')],
+          //   val: 'test2'
+          // }
+        //  (...)
+        ]
+      },
+      {
+        label: "China ",
+        data: [
+          {
+            timeRange: [-476, -221],
+            val: 'Warring States'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+          },
+          {
+            timeRange: [220, 280],
+            val: 'Three Kingdoms'
+          }
+          //,
+          // {
+          //   timeRange: [new Date('December 17, 1985 03:24:00'), new Date('December 19, 1992 03:24:00')],
+          //   val: 'test2'
+          // }
+        //  (...)
+        ]
+      },
+
+      //,
+    //   {
+    //     label: "label2name",
+    //     data:   [
+    //
+    //     {
+    //         timeRange: [new Date('January 17, 1995 03:24:00'), new Date('December 19, 1995 03:24:00')],
+    //         val: 'test3'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+    //       },
+    //       {
+    //         timeRange: [new Date('January 17, 1985 03:24:00'), new Date('May 19, 1992 03:24:00')],
+    //         val: 'test4'
+    //       }
+    //     //  (...)
+    //   ],
+    //   // (...)
+    // },
+  ],
+}
+
+
+]
+;
