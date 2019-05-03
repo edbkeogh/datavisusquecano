880aaa8fa21978acef13e0eb1305c9d58e790597
Edward Keogh <edbkeogh@gmail.com>
2019-05-03 08:53:44 -0500

    new timeline lanes

--- BAM/servertest/EM/timeline/dataformat22.js
+++ BAM/servertest/EM/timeline/dataformat22.js
@@ -50,12 +50,7 @@ var tldata = [
             timeRange: [1368, 1644],
             val: 'Ming'
           }
-          //,
-          // {
-          //   timeRange: [new Date('December 17, 1985 03:24:00'), new Date('December 19, 1992 03:24:00')],
-          //   val: 'test2'
-          // }
-        //  (...)
+
         ]
       },
       {
@@ -68,36 +63,410 @@ var tldata = [
           {
             timeRange: [220, 280],
             val: 'Three Kingdoms'
+          },
+          {
+            timeRange: [386, 589],
+            val: 'Norther and Southern Dynasties'
           }
-          //,
-          // {
-          //   timeRange: [new Date('December 17, 1985 03:24:00'), new Date('December 19, 1992 03:24:00')],
-          //   val: 'test2'
-          // }
-        //  (...)
+
         ]
       },
 
-      //,
-    //   {
-    //     label: "label2name",
-    //     data:   [
-    //
-    //     {
-    //         timeRange: [new Date('January 17, 1995 03:24:00'), new Date('December 19, 1995 03:24:00')],
-    //         val: 'test3'//<val: number (continuous dataScale) or string (ordinal dataScale)>
-    //       },
-    //       {
-    //         timeRange: [new Date('January 17, 1985 03:24:00'), new Date('May 19, 1992 03:24:00')],
-    //         val: 'test4'
-    //       }
-    //     //  (...)
-    //   ],
-    //   // (...)
-    // },
+
   ],
+},
+{
+  group: "Japan",
+  data: [
+    {
+      label: "Japan",
+      data: [
+        {
+          timeRange: [250, 538],
+          val: 'Kofun'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [538, 710],
+          val: 'Asuka'
+        },
+        {
+          timeRange: [710, 794],
+          val: 'Nara'
+        },
+        {
+          timeRange: [794, 1185],
+          val: 'Heian'
+        },
+        {
+          timeRange: [1185, 1333],
+          val: 'Kamakura'
+        },
+
+        {
+          timeRange: [1333, 1568],
+          val: 'Muromachi'
+        }
+      ]
+    }
+
+],
+},
+{
+  group: "Korea",
+  data: [
+    {
+      label: "Korea",
+      data: [
+        {
+          timeRange: [-57, 935],
+          val: 'Silla'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [918, 1392],
+          val: 'Goryeo'
+        },
+        {
+          timeRange: [1392, 1897],
+          val: 'Joseon'
+        }
+      ]
+    },
+    {
+      label: "Korea ",
+      data: [
+        {
+          timeRange: [-37, 668],
+          val: 'Goguryeo'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    },
+    {
+      label: "Korea  ",
+      data: [
+        {
+          timeRange: [-18, 660],
+          val: 'Baekje'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    }
+
+],
+},
+{
+  group: "India",
+  data: [
+    {
+      label: "India",
+      data: [
+        {
+          timeRange: [-322, -185],
+          val: 'Maurya'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [280, 550],
+          val: 'Gupta'
+        },
+        {
+          timeRange: [750, 1200],
+          val: 'Pala'
+        },
+        {
+          timeRange: [1206, 1526],
+          val: 'Delhi Sultanate'
+        },
+      ]
+    },
+    {
+      label: "India ",
+      data: [
+        {
+          timeRange: [-100, 225],
+          val: 'Satavahana'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [250, 500],
+          val: 'Vakataka'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [500, 1189],
+          val: 'Chalukyas'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    },
+    {
+      label: "India  ",
+      data: [
+        {
+          timeRange: [-280, 360],
+          val: 'Kushan'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [730, 1036],
+          val: 'Gurjara-Pratihara'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    },
+    {
+      label: "India   ",
+      data: [
+        {
+          timeRange: [-300, 1279],
+          val: 'Chola'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    },
+    {
+      label: "India    ",
+      data: [
+        {
+          timeRange: [275, 897],
+          val: 'Pallava'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    },
+    {
+      label: "India     ",
+      data: [
+        {
+          timeRange: [345, 540],
+          val: 'Kadambas'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [753, 982],
+          val: 'Rashtrakuta'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    },
+    {
+      label: "India      ",
+      data: [
+        {
+          timeRange: [350, 1140],
+          val: 'Kamarupa'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    }
+],
+},
+{
+  group: "Greece",
+  data: [
+    {
+      label: "Greece",
+      data: [
+        {
+          timeRange: [-2000, -1000],
+          val: 'Minoan'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [-1100, -800],
+          val: 'Dark Ages'
+        },
+        {
+          timeRange: [-800, -480],
+          val: 'Archaic'
+        },
+        {
+          timeRange: [-480, -323],
+          val: 'Classical'
+        },
+        {
+          timeRange: [-323, -146],
+          val: 'Hellenistic'
+        },
+
+        {
+          timeRange: [-146, 324],
+          val: 'Roman'
+        },
+        {
+          timeRange: [324, 1453],
+          val: 'Byzantine'
+        },
+        {
+          timeRange: [1453, 1821],
+          val: 'Ottoman'
+        }
+      ]
+    },
+    {
+      label: "Greece ",
+      data: [
+        {
+          timeRange: [-1600, -1100],
+          val: 'Mycenaean'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    },
+
+
+],
+},
+{
+  group: "Italy",
+  data: [
+    {
+      label: "Italy",
+      data: [
+        {
+          timeRange: [-753, -509],
+          val: 'Roman Kingdom'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [-509, -27],
+          val: 'Roman Republic'
+        },
+        {
+          timeRange: [-27, 476],
+          val: 'Roman Empire'
+        },
+        {
+          timeRange: [476, 553],
+          val: 'Ostrogothic Kingdom'
+        },
+        {
+          timeRange: [584, 751],
+          val: 'Exarchate of Ravenna'
+        },
+        {
+          timeRange: [754, 1870],
+          val: 'Papal States'
+        }
+      ]
+    },
+    {
+      label: "Italy ",
+      data: [
+        {
+          timeRange: [-900, -500],
+          val: 'Etruscan'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [568, 774],
+          val: 'Kingdom of the Lombards'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [774, 1250],
+          val: 'Holy Roman Empire'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    },
+    {
+      label: "Sicily",
+      data: [
+        {
+          timeRange: [831, 1091],
+          val: 'Emirate of Sicily'
+        },
+        {
+          timeRange: [1130, 1816],
+          val: 'Kingdom of Sicily'
+        }
+
+      ]
+    }
+
+],
+},
+{
+  group: "Eastern Europe",
+  data: [
+
+    {
+      label: "Eastern Europe ",
+      data: [
+        {
+          timeRange: [681, 1018],
+          val: 'First Bulgarian Empire'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [1185, 1396],
+          val: 'Second Bulgarian Empire'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+
+      ]
+    },
+    {
+      label: "Eastern Europe        ",
+      data: [
+    {
+      timeRange: [1136, 1478],
+      val: 'Novgorod Republic'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+    }
+  ]
+},
+    {
+      label: "Eastern Europe  ",
+      data: [
+        {
+          timeRange: [1199, 1253],
+          val: 'Kingdom of Galicia-Volhynia'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        },
+        {
+          timeRange: [1253, 1349],
+          val: 'Ruthenia'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    },
+    {
+      label: "Eastern Europe   ",
+      data: [
+        {
+          timeRange: [882, 1240],
+          val: 'Kievan Rus\''//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+      ]
+    },
+    {
+      label: "Eastern Europe    ",
+      data: [
+        {
+          timeRange: [1000, 1918],
+          val: 'Kingdom of Hungary'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    },
+    {
+      label: "Eastern Europe     ",
+      data: [
+        {
+          timeRange: [966, 1795],
+          val: 'Kingdom of Poland'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    },
+    {
+      label: "Eastern Europe      ",
+      data: [
+        {
+          timeRange: [1236, 1569],
+          val: 'Grand Duchy of Lithuania'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    },
+    {
+      label: "Eastern Europe",
+      data: [
+        {
+          timeRange: [1085, 1918],
+          val: 'Kingdom of Bohemia'//<val: number (continuous dataScale) or string (ordinal dataScale)>
+        }
+
+      ]
+    }
+],
 }
-
-
+//region
 ]
 ;
