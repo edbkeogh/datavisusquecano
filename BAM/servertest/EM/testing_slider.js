2ac59f5aed52b81a885b8755ad6b2b127d41ed42
edbkeogh <35552750+edbkeogh@users.noreply.github.com>
2019-05-15 14:39:07 -0500

    might need to tag out here
    resume with testing.html and testing_slider.js

--- BAM/servertest/EM/testing_slider.js
+++ BAM/servertest/EM/testing_slider.js
@@ -10,6 +10,22 @@ let paramTypes = ['languages', 'contexts', 'forms', 'materials'];
 let keys = ['id', 'manuscript', 'languages', 'geopolitical_context', 'terminus_post_quem', 'terminus_ante_quem', 'forms', 'materials'];
 let witnessKeys = ['id', 'author', 'terminus_post_quem', 'terminus_ante_quem', 'forms', 'materials', 'text'];
 
+var slider1 = document.getElementById("startDateslide");
+var output1 = document.getElementById("startDate");
+//output1.innerHTML = slider1.value;
+
+slider1.onchange = function() {
+  output1.value = this.value;
+}
+
+var slider2 = document.getElementById("endDateslide");
+var output2 = document.getElementById("endDate");
+//output2.innerHTML = slider2.value;
+
+slider2.onchange = function() {
+  output2.value = this.value;
+}
+
 function clearParams() {
 	// Clear the selected search parameters
 	searchData = {
